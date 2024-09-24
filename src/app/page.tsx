64d4d051c7e9615ef479997070905fcc4e3ca065
hrvoje vincek <vincekhrvoje@gmail.com>
2024-09-24 16:27:00 +0200

    comma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import React from "react";
-
-import { Loader } from "lucide-react";
-
 import NewsGrid from "@/components/NewsGrid";
 import { useGetTopStories } from "@/hooks/useGetTopStories";
 
